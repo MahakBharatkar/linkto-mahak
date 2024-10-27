c7cf4fd0f70709e2fcb2706ce1a3f3dd6ff3e841
Mahak Bharatkar <mahakbharatkar@Mahaks-MacBook-Air.local>
2024-10-27 20:44:06 +0530

    test: added harish-30

--- app/page.js
+++ app/page.js
@@ -13,7 +13,13 @@ const linkInfo = [
     link_text: "Visit online store | Calligraphy supplies",
     url: 'https://nyctophiles.myinstamojo.com/',
     image: "/mahak_insta.png",
+  },
+  {
+    link_text: "Harish-30",
+    url: 'https://fastrr-boost-ui.pickrr.com/?platform=SHOPIFY&fbp=fb.1.1717422565361.150236918&userDeviceId=73cd1c2f-4520-4a34-9e7c-099e30d25a48&userSessionId=73cd1c2f-4520-4a34-9e7c-099e30d25a48-1730041190667&cart=JTVCJTdCJTIycHJvZHVjdElkJTIyJTNBODAwNjE5MDEwNDgwMyUyQyUyMnRpdGxlJTIyJTNBJTIyU2hlJTIwQ2FyZSUyMEp1aWNlJTIwLSUyMDEwMDAlMjBtbCUyMCU3QyUyMFBhY2slMjBvZiUyMDElMjIlMkMlMjJ2YXJpYW50SWQlMjIlM0E0MzY1NzQwMzEzODI3NSUyQyUyMnZhcmlhbnRUaXRsZSUyMiUzQSUyMjEwMDAlMjBtbCUyMCU3QyUyMFBhY2slMjBvZiUyMDElMjIlMkMlMjJwcmljZSUyMiUzQTUwMCUyQyUyMnF1YW50aXR5JTIyJTNBMSUyQyUyMmltYWdlJTIyJTNBJTIyaHR0cHMlM0ElMkYlMkZjZG4uc2hvcGlmeS5jb20lMkZzJTJGZmlsZXMlMkYxJTJGMDU3MCUyRjkwNzAlMkYzNTU2JTJGZmlsZXMlMkZTaGVDYXJlSnVpY2UuanBnJTNGdiUzRDE3MjM1MzcxNjMlMjIlMkMlMjJpdGVtX21ldGFfZGF0YSUyMiUzQSU3QiU3RCUyQyUyMmN1c3RvbUF0dHJpYnV0ZXMlMjIlM0ElN0IlN0QlMkMlMjJ2ZW5kb3IlMjIlM0ElMjJLcmlzaG5hJ3MlMjBIZXJiYWwlMjAlMjYlMjBBeXVydmVkYSUyMiUyQyUyMnByb2R1Y3RfdHlwZSUyMiUzQSUyMkp1aWNlcyUyMiU3RCU1RA==&ipAddress=3.111.202.221&utm_params=dGltZV92aXNpdGVkPTIwMjQtMTAtMjdUMTU6MDA6MDQuMzU5Wg==&uuid=851b841e&channel=JTdCJTIyc2hvcF9uYW1lJTIyJTNBJTIyY29tcGFueS1sb2dvJTIyJTJDJTIyc2hvcF91cmwlMjIlM0ElMjJrcmlzaG5hYXl1cnZlZC5jb20lMjIlMkMlMjJyZWRpcmVjdFVybCUyMiUzQSUyMmh0dHBzJTNBJTJGJTJGa3Jpc2huYWF5dXJ2ZWQuY29tJTJGY2FydCUyRjQzNjU3NDAzMTM4Mjc1JTNBMSUyMiUyQyUyMmNyZWRJbnN0YWxsZWQlMjIlM0FmYWxzZSU3RA==&type=product',
+    image: "/mahak_insta.png",
   }
+
 ];
 
 export default function Home() {
